4ceccda030ec52ff3e0c5cc2f655ab47c1a23b14
Marponset <joel.marpons@mataro.epiaedu.cat>
2022-02-09 20:28:36 +0100

    Views de Login + introduir info de login fetes + boto de Login

--- app/src/main/java/com/jmarpons/projectefinalminimochis/MainActivity.java
+++ app/src/main/java/com/jmarpons/projectefinalminimochis/MainActivity.java
@@ -2,13 +2,26 @@ package com.jmarpons.projectefinalminimochis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btAnarLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btAnarLogin = findViewById(R.id.BTAnarLogin);
+
+        btAnarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
