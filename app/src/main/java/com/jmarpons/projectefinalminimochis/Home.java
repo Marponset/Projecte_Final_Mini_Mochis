83816db07572a98c090c8d848298f90450382f0a
Adria Castany Carranza <adriacastany@hotmail.es>
2022-02-21 20:56:09 +0100

    Faltanque els botons funcionin

--- app/src/main/java/com/jmarpons/projectefinalminimochis/Home.java
+++ app/src/main/java/com/jmarpons/projectefinalminimochis/Home.java
@@ -3,9 +3,12 @@ package com.jmarpons.projectefinalminimochis;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
@@ -18,20 +21,34 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-
         mBottomAppBar = findViewById(R.id.bottom_app_bar);
         setSupportActionBar(mBottomAppBar);
 
-        mBottomAppBar.setOnClickListener(new View.OnClickListener() {
+        mBottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId()==R.id.app_bar_games){
+                   Toast.makeText(Home.this, "hola", Toast.LENGTH_SHORT).show();
+                }
+                else if (item.getItemId()==R.id.app_bar_chat){
+                    Intent intent = new Intent(Home.this, Configuracio.class);
+                    //startActivity(intent);
+                } else if (item.getItemId()==R.id.app_bar_act){
+                    Intent intent = new Intent(Home.this, Configuracio.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+
+        /*mBottomAppBar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this, Configuracio.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
